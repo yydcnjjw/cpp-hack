b7b0c5cbfe8b10be3db020dadf848304c8c5eb51
jiangjw <jiangjw@koal.com>
2022-07-01 14:34:16 +0800

    update

--- rx/operator.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace rx {
-
-template <class T> struct operator_base {
-  using value_type = T;
-};
-
-} // namespace rx
--- rx/type.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <boost/system/error_code.hpp>
-
-namespace rx {
-
-using error_type = boost::system::error_code;
-
-} // namespace rx
