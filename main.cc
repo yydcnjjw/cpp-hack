16549897cc52cd6e99184bf96a033c91c2a2904d
jiangjw <jiangjw@koal.com>
2022-09-23 16:39:38 +0800

    update

--- main.cc
+++ main.cc
@@ -1,3 +1,15 @@
+#include <iostream>
+#include <map>
+
+struct A {
+  A() { inst_ = this; }
+
+  static A *inst_;
+};
+A * A::inst_ = nullptr;
+
 int main(int, char *[]) {
+  static A a;
+  std::cout <<  a.inst_ << std::endl;
   return 0;
 }
